1e9b731a8aeb3098512b3787e1ab84aff48e7582
yirga <yirga.liben@gmail.com>
2021-01-20 22:18:46 -0500

    update test

--- src/DemoAPI/Controllers/ValuesControllers.cs
+++ src/DemoAPI/Controllers/ValuesControllers.cs
@@ -18,7 +18,7 @@ namespace DemoAPI.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/values/1
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
