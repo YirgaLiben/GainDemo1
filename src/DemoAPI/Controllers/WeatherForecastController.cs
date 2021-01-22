981f4d7d4444deadf76b9d6f36fc02a486945e8a
yliben <yirega@yahoo.com>
2021-01-21 22:36:25 -0500

    update test ci

--- src/DemoAPI/Controllers/WeatherForecastController.cs
+++ src/DemoAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace DemoAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering","xxx"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
